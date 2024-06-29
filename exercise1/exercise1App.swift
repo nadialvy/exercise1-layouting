8ac302aaf7589d99cbfa167545a59841730b7187
nadialvy <5026231007@student.its.ac.id>
2024-06-30 04:55:28 +0700

    Initial Commit

---
+++ exercise1/exercise1App.swift
@@ -0,0 +1,17 @@
+//
+//  exercise1App.swift
+//  exercise1
+//
+//  Created by Nadia Lovely on 30/06/24.
+//
+
+import SwiftUI
+
+@main
+struct exercise1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
